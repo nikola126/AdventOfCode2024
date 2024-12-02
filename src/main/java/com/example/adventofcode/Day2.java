57de6801d25f88563386bb6c6ac566ae64423118
Nikola <nikj1705@gmail.com>
2024-12-02 20:45:02 +0200

    Day 2 Part 2 Complete

--- src/main/java/com/example/adventofcode/Day2.java
+++ src/main/java/com/example/adventofcode/Day2.java
@@ -12,6 +12,7 @@ public class Day2 {
 
     public Day2() {
         partOne();
+        partTwo();
     }
 
     @SneakyThrows
@@ -24,42 +25,70 @@ public class Day2 {
             String[] tokens = line.split(" ");
 
             List<Integer> levels = Arrays.stream(tokens).map(Integer::parseInt).toList();
-            boolean allIncreasing = true;
-            boolean allDecreasing = true;
-            boolean allSafe = true;
 
-            for (int i = 1; i < levels.size(); i++) {
-                int diff = levels.get(i - 1) - levels.get(i);
-                int absDiff = Math.abs(diff);
-
-                boolean isIncreasing = diff > 0;
-                boolean isDecreasing = diff < 0;
-                boolean isDiffSafe = absDiff >= 1 && absDiff <= 3;
-
-                if (!isDiffSafe) {
-                    allSafe = false;
-                }
-
-                if (diff == 0) {
-                    allIncreasing = false;
-                    allDecreasing = false;
-                }
-
-                if (!isIncreasing) {
-                    allIncreasing = false;
-                }
-
-                if (!isDecreasing) {
-                    allDecreasing = false;
-                }
-            }
-
-            if (allSafe && (allIncreasing || allDecreasing)) {
+            if (isReportSafe(levels)) {
                 countSafe += 1;
             }
         }
 
         System.out.println("Day 2 Part 1: " + countSafe);
+    }
+
+    @SneakyThrows
+    public void partTwo() {
+        scanner = new Scanner(file);
+        int countSafe = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+
+            List<Integer> levels = Arrays.stream(tokens).map(Integer::parseInt).toList();
+
+            if (isReportSafe(levels)) {
+                countSafe += 1;
+            } else {
+                for (int i = 0; i < levels.size(); i++) {
+                    List<Integer> shortened = new ArrayList<>(levels);
+                    shortened.remove(i);
+
+                    if (isReportSafe(shortened)) {
+                        countSafe += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Day 2 Part 2: " + countSafe);
+    }
+
+    private boolean isReportSafe(List<Integer> levels) {
+        boolean allIncreasing = true;
+        boolean allDecreasing = true;
+        boolean allSafe = true;
+
+        for (int i = 1; i < levels.size(); i++) {
+            int diff = levels.get(i - 1) - levels.get(i);
+            int absDiff = Math.abs(diff);
+
+            boolean isIncreasing = diff > 0;
+            boolean isDecreasing = diff < 0;
+            boolean isDiffSafe = absDiff >= 1 && absDiff <= 3;
+
+            if (!isDiffSafe) {
+                allSafe = false;
+            } else if (diff == 0) {
+                allIncreasing = false;
+                allDecreasing = false;
+            } else if (!isIncreasing) {
+                allIncreasing = false;
+            } else if (!isDecreasing) {
+                allDecreasing = false;
+            }
+        }
+
+        return allSafe && (allIncreasing || allDecreasing);
     }
 
 }
